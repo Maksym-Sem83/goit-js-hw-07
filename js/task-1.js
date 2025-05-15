8e3f2a22bd5333e2cf2f661fde0e9cebaeb08f9a
Maksym Semenov <max_semenov@ukr.net>
2025-05-15 22:12:44 +0300

    add task-1 change

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,10 @@
-const numberCat = document.querySelectorAll(".item");
-console.log(`Number of categories: ${numberCat.length}`);
+const numberCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${numberCategories.length}`);
 
-numberCat.forEach(category => { });
+numberCategories.forEach(category => {
+    const titleCategories = category.querySelector(".item-title").textContent;
+    const elementCategories = category.querySelectorAll(".categories-list-item").length;
+    console.log(`Category: ${titleCategories}`);
+    console.log(`Elements: ${elementCategories}`);
+    
+ });
